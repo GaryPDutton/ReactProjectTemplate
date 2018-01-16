999f4180de3cc256168eaf7930d12d47f3b93ec8
Gary Dutton <garydutton@garys-mbp-2.home>
2018-01-16 15:02:50 +0000

    Initial commit, code from react course on pluralsight

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>About demo application</h1>
+                <p>Uses react, redux, react router and other cool stuff.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+import courses from './courseReducer';
+import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
+
+const routeReducer = combineReducers({
+    courses,
+    authors,
+    ajaxCallsInProgress
+});
+
+export default routeReducer;
